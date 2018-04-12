3bf0d8fc2741fd987b02e9457b6283f3613c3ea6
Kazu <kazuhiko1979@gmail.com>
2018-04-12 04:34:49 +0000

    20180412

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,18 +2,25 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(created_at: :desc)
   end
-  
+
   def show
     @post = Post.find_by(id: params[:id])
   end
   
   def new
+    # Postクラスのインスタンスを作成し、変数@postに代入
+    @post = Post.new
   end
   
   def create
     @post = Post.new(content: params[:content])
-    @post.save
-    redirect_to("/posts/index")
+    # 保存に成功した場合は投稿一覧ページ、保存に失敗した場合は新規投稿ページが表示
+    if @post.save
+      flash[:notice] = "投稿を作成しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
   end
   
   def edit
@@ -23,13 +30,20 @@ class PostsController < ApplicationController
   def update
     @post = Post.find_by(id: params[:id])
     @post.content = params[:content]
-    @post.save
+    if @post.save
+    flash[:notice] = "投稿を編集しました"
     redirect_to("/posts/index")
+    else
+    # renderメソッドを用いて、editアクションを経由せず、posts/edit.html.erbが表示されるようにしてください
+    render("/posts/edit")
+    #redirect_to("/posts/#{@post.id}/edit")
+    end
   end
   
   def destroy
     @post = Post.find_by(id: params[:id])
     @post.destroy
+    flash[:notice] = "投稿を削除しました"
     redirect_to("/posts/index")
   end
   
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
+  # contentカラムに対して、空の投稿を制限するバリデーション
+  validates :content, {presence: true, length: {maximum: 140}}
   
 end
